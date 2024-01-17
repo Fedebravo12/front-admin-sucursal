419df49d60b56eee58a41da234e970fc1801a2a5
German Salinas <germansalinas.fce@gmail.com>
2024-01-17 16:30:43 -0300

    cambios reporte

--- src/pages/Sucursal/ListadoPedidos.jsx
+++ src/pages/Sucursal/ListadoPedidos.jsx
@@ -113,29 +113,72 @@ const ListadoPedidos = () => {
     }
 
     const generarReporte = async () => {
-        // const mes = parseInt(fechaSeleccionada.format('MM'));
-        // const anio = parseInt(fechaSeleccionada.format('YYYY'));
-        // const estado = parseInt(estadoEnvio);
-        // const body = {
-        //     mes,
-        //     anio,
-        //     estado
-        // }
-        // try {
-        //     debugger;
-        //     showLoadingModal();
-        //     const token = localStorage.getItem('token');
-        //     const headers = {
-        //         Authorization: `Bearer ${token}`
-        //     };
-        //     const response = await axios.post(apiLocalKey + "/generarReportePedidosSucursal", body, { headers });
-        //     hideLoadingModal();
-        // }
-        // catch (error) {
-        //     hideLoadingModal();
+        try {
+            showLoadingModal();
+            const token = localStorage.getItem('token');
+            const headers = {
+                Authorization: `Bearer ${token}`,
 
-        // }
+            };
+
+            const body = {
+                mes: parseInt(fechaSeleccionada.format('MM')),
+                anio: parseInt(fechaSeleccionada.format('YYYY')),
+        
+            }
+
+            const response = await axios.post(apiLocalKey + "/generarReportePedidosSucursal", body, { headers });
+
+            // La API envuelve la respuesta en un objeto, así que necesitamos acceder a la propiedad 'result' y luego a 'pdf'
+            const pdfBase64 = response.data.result.pdf;
+            const nombre = response.data.result.nombre;
+
+            debugger;
+
+            // Convierto la cadena base64 a un Blob, llamo a la función base64ToBlob
+            const pdfBlob = base64ToBlob(pdfBase64, 'application/pdf');
+
+            // Creo un enlace URL para el archivo
+            const url = window.URL.createObjectURL(pdfBlob);
+
+            // Creo un enlace nuevo en el DOM para descargar el archivo
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', nombre); // Nombre del archivo para descargar
+            document.body.appendChild(link);
+            link.click();
+
+            // Limpiar y remover el enlace del DOM
+            link.parentNode.removeChild(link);
+            window.URL.revokeObjectURL(url);
+
+            hideLoadingModal();
+        } catch (error) {
+            console.error("Error al descargar la factura: ", error);
+            hideLoadingModal();
+        }
+
+
     }
+
+    const base64ToBlob = (base64, mimeType) => {
+        //llamo a la función atob para decodificar la cadena base64, atob es una función nativa de JavaScript
+        const byteCharacters = atob(base64);
+        //construyo un array de bytes a partir de la cadena decodificada
+        const byteNumbers = new Array(byteCharacters.length);
+        //recorro el array de bytes y los lleno con los valores de la cadena decodificada
+        for (let i = 0; i < byteCharacters.length; i++) {
+            byteNumbers[i] = byteCharacters.charCodeAt(i);
+        }
+        //convierto el array de bytes en un int8Array que es una clase nativa de JavaScript para manejar arrays de bytes
+        const byteArray = new Uint8Array(byteNumbers);
+        //convierto el int8Array en un objeto Blob, que es el tipo de objeto que acepta la propiedad href del enlace
+        return new Blob([byteArray], {type: mimeType});
+    };    
+
+
+
+
 
 
 
