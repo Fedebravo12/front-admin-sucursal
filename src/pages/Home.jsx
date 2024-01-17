0d747c47c8a521d8d8379016be19ecffc23bd988
German Salinas <germansalinas.fce@gmail.com>
2024-01-17 19:36:42 -0300

    cambios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
+import { useAuth0 } from '@auth0/auth0-react';
 
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+    const { isAuthenticated, user} = useAuth0();
+    const rol_admin = import.meta.env.VITE_APP_ROLE_ADMIN;
+    const rol_sucursal = import.meta.env.VITE_APP_ROLE_SUCURSAL;
+
+    console.log(user)
+
+    if (isAuthenticated && user.rol_usuario[0] == rol_admin) {
+        return (
+            <div>
+                <h1>Home Admin</h1>
+            </div>
+
+        )
+    }
+    if (isAuthenticated && user.rol_usuario[0] == rol_sucursal) {
+        return (
+            <div>
+                <h1>Home Sucursal</h1>
+            </div>
+
+        )
+    }
+
 }
 export default Home;
